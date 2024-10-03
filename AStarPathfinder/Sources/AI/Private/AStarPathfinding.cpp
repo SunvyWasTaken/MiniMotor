efa6be692547ba6310ed054d959570dd0b6541b9
DevOfSomewhere <keanu.vongue@gmail.com>
2024-10-03 10:58:53 +0200

    Update AStarPathfinding.cpp
    Pour le stream

--- AStarPathfinder/Sources/AI/Private/AStarPathfinding.cpp
+++ AStarPathfinder/Sources/AI/Private/AStarPathfinding.cpp
@@ -39,57 +39,7 @@ namespace PathFinderAlgo
 			return;
 		}
 
-		OpenList.push_back(Start);
-
-		while (!OpenList.empty())
-		{
-			Unit<Path>* current = OpenList[0];
-			for (auto& cell : OpenList)
-			{
-				if (cell->hCost < current->hCost)
-				{
-					current = cell;
-				}
-			}
-
-			OpenList.erase(std::remove(OpenList.begin(), OpenList.end(), current), OpenList.end());
-			CloseList.push_back(current);
-
-			if (current == End)
-			{
-				for (auto& curr : CloseList)
-				{
-					parent->GetVertexArray()[curr->pos.x * parent->MazeSize.x + curr->pos.y].FillColor({ 255, 0, 0 });
-				}
-				while (current != Start)
-				{
-					parent->GetVertexArray()[current->pos.x * parent->MazeSize.x + current->pos.y].FillColor({0, 255, 0});
-					current = current->beforePath;
-				}
-				return;
-			}
-
-			for (auto& neighbour : current->GetNeighbours())
-			{
-				if (std::find(CloseList.begin(), CloseList.end(), neighbour) != CloseList.end())
-				{
-					continue;
-				}
-
-				uint64_t newCostToNeighbour = (neighbour->pos - current->pos).length() + current->gCost;
-				if (newCostToNeighbour < neighbour->gCost || std::find(OpenList.begin(), OpenList.end(), neighbour) == OpenList.end())
-				{
-					neighbour->gCost = newCostToNeighbour;
-					neighbour->hCost = (End->pos - neighbour->pos).length();
-					neighbour->beforePath = current;
-
-					if (std::find(OpenList.begin(), OpenList.end(), neighbour) == OpenList.end())
-					{
-						OpenList.push_back(neighbour);
-					}
-				}
-			}
-		}
+		// A* algorithm
 	}
 
 	DijkstraPathfinding::DijkstraPathfinding(MazeTerrain* _parent)
