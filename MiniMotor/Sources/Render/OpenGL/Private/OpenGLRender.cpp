8d0b7d33651bb11694f5015eb5562e9a91d84756
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2025-02-19 19:09:18 +0100

    imgui

--- MiniMotor/Sources/Render/OpenGL/Private/OpenGLRender.cpp
+++ MiniMotor/Sources/Render/OpenGL/Private/OpenGLRender.cpp
@@ -6,12 +6,12 @@
 #include "OpenGLShader.h"
 #include "Typelists.h"
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 
 #define NBR_POINT_LIGHT 4
 
